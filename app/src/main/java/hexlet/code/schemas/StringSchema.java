9d9849c82adadf0bcd754bd92ed9eacff80de656
Lakever <givster@yandex.ru>
2025-07-08 19:13:06 +0300

    Update StringSchema.java

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -20,18 +20,4 @@ public class StringSchema extends BaseSchema<String> {
         rules.add(value -> value != null && value.contains(substr));
         return this;
     }
-
-//    public boolean isValid(String value) {
-//        // Если required не вызывался, то null и пустая строка — допустимы
-//        if (!isRequired && (value == null || value.isEmpty())) {
-//            return true;
-//        }
-//        // Применяем все правила
-//        for (Predicate<String> rule : rules) {
-//            if (!rule.test(value)) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
 }
